763f660775dcb39e2f89885685999189ce7d6e98
Sushant-saud <98227002+Sushant-saud@users.noreply.github.com>
2022-11-22 12:22:03 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -27,17 +27,4 @@ function App() {
 }
 
 export default App;
-// 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
-// git remote add origin https://github.com/Sushant-saud/test.git
-// git branch -M main
-// git push -u origin 
-// git init
-// git add README.md
-// git commit -m "first commit"
-// git branch -M main
-// git remote add origin https://github.com/Sushant-saud/test.git
-// git push -u origin main
-// …or push an existing repository from the command line
-// git remote add origin https://github.com/Sushant-saud/test.git
-// git branch -M main
-// git push -u origin main
+
