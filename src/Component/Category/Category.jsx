57422b5c49b6c9b201cf7a84515272bf742c4281
Sushant-saud <98227002+Sushant-saud@users.noreply.github.com>
2022-11-22 23:40:32 +0530

    Update Category.jsx

--- src/Component/Category/Category.jsx
+++ src/Component/Category/Category.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import cate from './cate.css'
-function Category({data1,data2}){
+function Category({data1}){
   return (
     <>
    
           <div className='cat'>
                 <p className='travels'>{data1.category}</p>
-                <p className='dates'> / {new Date(data2.createdAt).toDateString()}</p>
+                <p className='dates'> / {new Date(data1.createdAt).toDateString()}</p>
             </div>
      
     </>
